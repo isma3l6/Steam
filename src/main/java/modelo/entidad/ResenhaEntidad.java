11b3bd778f3c565f1b944724f4089838744236de
Ismael Fernandez Fernandez <a23ismaelff@teis.intranet>
2026-02-20 16:59:11 +0100

    merge formulario

--- src/main/java/modelo/entidad/ResenhaEntidad.java
+++ src/main/java/modelo/entidad/ResenhaEntidad.java
@@ -3,12 +3,6 @@ package modelo.entidad;
 import java.util.Date;
 
 public class ResenhaEntidad {
-    private int id;
-    private String idusuaro;
-    private String idJuego;
-    private boolean recomendado;
-    private String texto;
-    private Date fechaPublicacion;
-    private Date fechaEdit;
+}
 
 
